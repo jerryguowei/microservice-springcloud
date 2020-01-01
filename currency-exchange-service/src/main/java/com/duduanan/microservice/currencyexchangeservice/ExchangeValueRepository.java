68cede977891c555903c3dd0ea986d60dd8746c7
jerryguowei <jerryguowei@gmail.com>
2020-01-02 07:48:22 +1100

    first commit

---
+++ currency-exchange-service/src/main/java/com/duduanan/microservice/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.duduanan.microservice.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	public ExchangeValue findByFromAndTo(String from, String to);
+}
